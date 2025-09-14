157d07d0fa3283af7ef3885d835477207f21ac6a
joshuacyriacus <joshuacyriacus8@gmail.com>
2025-09-14 21:06:18 +0100

    image section removed

--- src/components/propertyC.jsx
+++ src/components/propertyC.jsx
@@ -2,24 +2,13 @@ import { useState } from "react";
 import { MapPin, Home } from "lucide-react";
 import ConfirmAndPay from "./ConfirmAndPay";
 
-function Property({ title, price, location, image }) {
+function Property({ title, price, location }) {
   const [open, setOpen] = useState(false);
 
   return (
     <>
       <div className="p-4 rounded-lg ring-2 ring-green-500 shadow-xl flex flex-col transition-transform transform hover:scale-105 duration-200">
-        {/* Property Image */}
-        {image ? (
-          <img
-            src={image}
-            alt={title}
-            className="w-full h-48 object-cover rounded-lg mb-3"
-          />
-        ) : (
-          <div className="w-full h-48 bg-gray-200 rounded-lg flex items-center justify-center text-gray-500">
-            No Image Available
-          </div>
-        )}
+        
 
         {/* Title + Location */}
         <h2 className="text-xl font-bold flex items-center gap-2 truncate">
